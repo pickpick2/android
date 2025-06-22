e2e23af3866f521960b4678f3d8f2bef2b1c671c
권민채 <minchae075@naver.com>
2025-06-22 18:40:33 +0900

    ✨ feat: 프로필, 닉네임 컴포넌트 추가

--- app/src/main/java/com/pickpick/pickpick/presentation/info/InfoScreen.kt
+++ app/src/main/java/com/pickpick/pickpick/presentation/info/InfoScreen.kt
@@ -1,26 +1,57 @@
 package com.pickpick.pickpick.presentation.info
 
+import android.R.attr.fontStyle
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.pickpick.pickpick.R
 import com.pickpick.pickpick.core.ui.component.MainButton
 import com.pickpick.pickpick.core.ui.component.UnderlineTextField
 import com.pickpick.pickpick.core.ui.theme.Border
+import com.pickpick.pickpick.core.ui.theme.PrimaryDefault
+import com.pickpick.pickpick.core.ui.theme.Typography
+import com.pickpick.pickpick.core.ui.theme.font.PyeojinGothicFallback
+import com.pickpick.pickpick.core.ui.theme.font.PyeojinGothicTypography.Body1Regular
 import com.pickpick.pickpick.core.ui.theme.font.PyeojinGothicTypography.DetailRegular
+import com.pickpick.pickpick.core.ui.theme.font.PyeojinGothicTypography.Heading1
+import com.pickpick.pickpick.presentation.info.component.InfoTitleContent
 import com.pickpick.pickpick.presentation.info.viewmodel.InfoViewModel
 
 @Composable
@@ -41,17 +72,57 @@ fun InfoScreen(
         verticalArrangement = Arrangement.Center
     ) {
 
+        InfoTitleContent()
+
+        Spacer(modifier = Modifier.height(70.dp))
+
+        Box(
+            modifier = Modifier.size(150.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_default_profile),
+                contentDescription = "프로필 이미지",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.icon_pencil),
+                contentDescription = "프로필 수정",
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(Color.White, CircleShape)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
         UnderlineTextField(
             value = uiState.value.nickname, onValueChange = viewModel::updateNickname,
             placeholder = {
                 Text(
                     text = stringResource(R.string.nickname_placeholder),
-                    style = DetailRegular.copy(color = Border)
+                    style = Body1Regular.copy(
+                        color = Border,
+                        textAlign = TextAlign.Center
+                    ),
+                    modifier = Modifier.fillMaxWidth().padding(bottom = 14.dp)
                 )
             },
+            textStyle = TextStyle(
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = PyeojinGothicFallback,
+                fontWeight = FontWeight.Medium
+            ),
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
             errorText = uiState.value.nicknameErrorText,
         )
+
+        Spacer(modifier = Modifier.height(70.dp))
 
         MainButton(
             text = stringResource(R.string.signup_complete),
